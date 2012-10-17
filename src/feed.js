7545bc3eb30b098eb9de9eb382cc227d150ddd6e
kimura <gaoweiwukan@gmail.com>
2012-10-17 12:27:13 +0800

    feed类的东东

---
+++ src/feed.js
@@ -0,0 +1,39 @@
+var Hall = Hall || {};
+Hall.feed= {};
+(function(_){
+	_.bind = function(){
+		$("[jid='block']").live('click',function(){
+
+		});
+		$("[jid='trans']").live('click',function(){
+
+		});
+		$("[jid='save']").live('click',function(){
+
+		});
+		$("[jid='pass']").live('click',function(){
+
+		});
+	};
+	
+})(Hall.msgBox);
+
+Hall.feed= {};
+(function(_){
+	_.bind = function(){
+		$("[jid='block']").live('click',function(){
+
+		});
+		$("[jid='trans']").live('click',function(){
+
+		});
+		$("[jid='save']").live('click',function(){
+
+		});
+		$("[jid='pass']").live('click',function(){
+
+		});
+	};
+	
+})(Hall.feed);
+
